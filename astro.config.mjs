f0ca4045c0341d116c7acdf101f96e030ae96d1a
GarkarAXO <e.axel12@gmail.com>
2026-03-04 10:30:21 -0600

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,5 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
+  output: 'static', // Genera HTML puro, ideal para rendimiento y muestras
 });
